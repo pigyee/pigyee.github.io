17f1a299d308d969e9d8983ad5a98cf6e06a1179
pig810 <pig810@outlook.com>
2024-08-18 17:18:14 +0800

    update

--- js/head.js
+++ js/head.js
@@ -5,6 +5,16 @@ document.writeln("    <script src=\'https://cdn.staticfile.net/twitter-bootstrap
 document.writeln("    <link href=\'https://cdn.staticfile.net/bootstrap-icons/1.11.3/font/bootstrap-icons.min.css\' rel=\'stylesheet\'>");
 document.writeln("    <script src=\'/js/dark.js\'></script>");
 document.writeln("    <link rel=\'manifest\' href=\'/manifest.json\' />");
+document.writeln("<!-- 手机适配标识 -->");
+document.writeln("<meta name=\'apple-mobile-web-app-capable\' content=\'yes\'>");
+document.writeln("<meta name=\'mobile-web-app-capable\' content=\'yes\'>");
+document.writeln("<!-- 配置主题颜色 -->");
+document.writeln("<meta name=\'theme-color\' content=\'#ffffff\'>");
+document.writeln("<!-- 配置添加到主屏幕时的标题 -->");
+document.writeln("<meta name=\'apple-mobile-web-app-title\' content=\'猪圈\'>");
+document.writeln("<!-- 配置添加到主屏幕时的icon -->");
+document.writeln("<link rel=\'apple-touch-icon-precomposed\' sizes=\'512x512\' href=\'/favicon.ico\'>");
+document.writeln("<link rel=\'apple-touch-icon\' sizes=\'512x512\' href=\'/favicon.ico\'>");
 document.writeln("    <!-- 灯箱脚本 -->");
 document.writeln("    <script src=\'/js/view-image.min (1).js\'></script>");
 document.writeln("    <script>");
