7f4d10e943bf7c544968e9cb649dc3f747ee171e
pig810 <136228019+pigyee@users.noreply.github.com>
2024-09-17 19:19:54 +0800

    update js/head.js

--- js/head.js
+++ js/head.js
@@ -1,7 +1,7 @@
 document.writeln("<meta charset=\'utf-8\'>");
 document.writeln("    <meta name=\'viewport\' content=\'width=device-width, initial-scale=1\'>");
-document.writeln("    <link href=\'https://cdn.staticfile.net/twitter-bootstrap/5.3.2/css/bootstrap.min.css\' rel=\'stylesheet\'>");
-document.writeln("    <script src=\'https://cdn.staticfile.net/twitter-bootstrap/5.3.2/js/bootstrap.min.js\'></script>");
+document.writeln("<script src=\'https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/5.3.3/js/bootstrap.min.js\'></script>");
+document.writeln("<link href=\'https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/5.3.3/css/bootstrap.min.css\' rel=\'stylesheet\'>");
 document.writeln("    <link href=\'https://cdn.staticfile.net/bootstrap-icons/1.11.3/font/bootstrap-icons.min.css\' rel=\'stylesheet\'>");
 document.writeln("    <script src=\'/js/dark.js\'></script>");
 document.writeln("    <link rel=\'manifest\' href=\'/manifest.json\' />");
